1ec17ebf35fe47fbda434d7c074c90e8eac807bb
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-05 14:16:48 +0100

    Fetch Chain
    ugh

--- docs/code.js
+++ docs/code.js
@@ -1,16 +1,19 @@
-console.log('i work');
+
 
 let imgSrc;
 
-console.log('Start');
+
 fetch('img/coffie.jpg').
-then(function(response){console.log(`I actualy work lmoa XD ${response}`);
+then(function(response){console.log('Start');
+
 return response.blob();})
 .then(function(myBlob){
     let objectURL = URL.createObjectURL(myBlob);
     imgSrc = document.createElement('img');
     imgSrc.src = objectURL;
     document.body.appendChild(imgSrc);
+    console.log(`I actualy work lmoa XD ${response}`);
 })
+.then(function(){console.log('End');})
 .catch(function(error){console.log(`I faled u loser ${error}`);});
-console.log('End');
+
