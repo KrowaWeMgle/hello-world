d569cc386e2c8af8763291809e95fddae8431979
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-05 14:37:07 +0100

    Edit me baby

---
+++ docs/js/promiseCode.js
@@ -0,0 +1,15 @@
+//promise https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Promises
+
+//Explaining basic promise syntax: A real example
+
+let promise = fetch('img/coffie.jpg');
+let promise2 = promise.then(response => {
+    if(!response.ok){throw new Error(`HTTP error! status: ${response.status}`);}
+    else {return response.blob();}
+});
+let promise3 = promise2.then(myBlob => {
+    let objectURL = URL.createObjectURL(myBlob);
+    let image = document.createElement('img');
+    image.src = objectURL;
+    document.body.appendChild(image);
+})
