a06783ba41d8fd76f440e1e483683f5599a53ab5
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-05 15:00:10 +0100

    pls let mi in

--- docs/js/promiseCode.js
+++ docs/js/promiseCode.js
@@ -16,3 +16,33 @@ let promise3 = promise2.then(myBlob => {
 let errorCase = promise3.catch(e => {
     console.log(`There has been a problem with your fetch`);
 });
+
+//re-write primise.all
+let a = fetch('img/coffie');
+let b = fetch('img/coffie');
+let c = fetch('img/coffie');
+
+//Promise.all([a,b,c]).then(value => {});
+
+function fetchAndDecode(url, type) {
+    return fetch(url).then(response => {
+      if(!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      } else {
+        if(type === 'blob') {
+          return response.blob();
+        } else if(type === 'text') {
+          return response.text();
+        }
+      }
+    })
+    .catch(e => {
+      console.log(`There has been a problem with your fetch operation for resource "${url}": ` + e.message);
+    });
+  }
+
+let coffee = fetchAndDecode('coffee.jpg', 'blob');
+let tea = fetchAndDecode('tea.jpg', 'blob');
+let description = fetchAndDecode('description.txt', 'text');
+
+Promise.all
