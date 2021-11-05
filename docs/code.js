aa3e3ebe946fafa99454c8f8fd6b733a8e07d2fd
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-05 14:07:48 +0100

    Fetch edit
    its a long one

--- docs/code.js
+++ docs/code.js
@@ -1,1 +1,16 @@
 console.log('i work');
+
+let imgSrc;
+
+console.log('Start');
+fetch('img/coffie.jpg').
+then(function(response){console.log(`I actualy work lmoa XD ${response}`);
+return response.blob();})
+.then(function(myBlob){
+    let objectURL = URL.createObjectURL(myBlob);
+    imgSrc = document.createElement('img');
+    imgSrc.src = objectURL;
+    document.body.appendChild(imgSrc);
+})
+.catch(function(error){console.log(`I faled u loser ${error}`);});
+console.log('End');
